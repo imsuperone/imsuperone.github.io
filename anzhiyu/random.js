a9d6896c986ff14b19fd1f9d212d24d6843fb6e8
faxlight <fax.light233@gmail.com>
2025-01-17 15:32:40 +0800

    Site updated: 2025-01-17 15:32:39

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["a/37083","a/14991"];function toRandomPost(){
+var posts=["a/37083","a/958a0988","a/14991"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
