30ac70a551e9a6e349242ac374a2630fbfb1cbd7
faxlight <fax.light233@gmail.com>
2025-01-18 19:48:42 +0800

    Site updated: 2025-01-18 19:48:40

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["37083","cad832a4","958a0988","14991"];function toRandomPost(){
+var posts=["37083","f41b8e08","cad832a4","958a0988","14991"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
