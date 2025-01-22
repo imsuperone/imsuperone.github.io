79a4afb12c9bc05712a188916724438ff0299480
faxlight <fax.light233@gmail.com>
2025-01-22 12:35:44 +0800

    Site updated: 2025-01-22 12:35:43

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["post-e2fce66f/","post-1de292a1/","post-f41b8e08/","post-cad832a4/","post-958a0988/","post-37083/","post-14991/"];function toRandomPost(){
+var posts=["post-37636c7d/","post-e2fce66f/","post-f41b8e08/","post-37083/","post-14991/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
